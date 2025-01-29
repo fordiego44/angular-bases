979084865b5d2680afb8353bb690c3f894ad90c6
Diego Armando <fordiego44@gmail.com>
2025-01-28 21:13:03 -0500

    seccion 6

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output  } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+  standalone: false,
+})
+export class ListComponent {
+
+
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+  @Output()
+  public onDeletec: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter( id: string):void{
+    // console.log("INDEX-LIST ",index);
+    console.log("id: ", id);
+
+    this.onDeletec.emit(id);
+  }
+}
